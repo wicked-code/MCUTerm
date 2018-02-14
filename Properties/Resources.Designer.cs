cfd97d6d0668f3143d0bfaf19a9711d71fc695d1
wicked-code <WickedCode@mail.ru>
2018-02-14 22:58:09 +0300

    Change window icon

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace MCUTerm.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap terminal {
-            get {
-                object obj = ResourceManager.GetObject("terminal", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
